fbd1572a11930f31e3d0ad5c5b6cf2a9db188bd7
ciwik <lgnv2009@gmail.com>
2018-06-14 00:35:18 +0400

    Add tests

---
+++ GZipLibrary.Tests/BlockQueueTest.cs
@@ -0,0 +1,56 @@
+﻿using GZipLibrary.Blocks;
+using NUnit.Framework;
+
+namespace GZipTest.Tests
+{
+    [TestFixture]
+    public class BlockQueueTest
+    {
+        [Test]
+        public void EnqueueDequeueOneBlock()
+        {
+            //Arrange
+            int queueLength = 3;
+            var queue = new BlockQueue(queueLength);
+            Block inputBlock = new Block(1, new byte[] { 1, 1, 2, 3, 5, 8 });
+            Block outputBlock;          
+
+            //Act
+            queue.Enqueue(inputBlock);            
+            bool result = queue.TryDequeue(out outputBlock);
+
+            //Assert
+            Assert.True(result);
+            Assert.AreEqual(inputBlock, outputBlock);
+        }
+
+        [Test]
+        public void EnqueueDequeueManyBlocks()
+        {
+            //Arrange
+            int queueLength = 3;
+            var queue = new BlockQueue(queueLength);
+            Block inputBlock1 = new Block(1, new byte[] {1, 1, 2, 3, 5, 8});
+            Block inputBlock2 = new Block(2, new byte[] {13, 21});
+            Block inputBlock3 = new Block(3, new byte[] {34, 55, 89});
+
+            //Act
+            queue.Enqueue(inputBlock1);
+            queue.Enqueue(inputBlock2);
+            queue.Enqueue(inputBlock3);
+
+            bool result1 = queue.TryDequeue(out var outputBlock1);
+            bool result2 = queue.TryDequeue(out var outputBlock2);
+            bool result3 = queue.TryDequeue(out var outputBlock3);
+
+            //Assert
+            Assert.True(result1);
+            Assert.True(result2);
+            Assert.True(result3);
+
+            Assert.AreEqual(inputBlock1, outputBlock1);
+            Assert.AreEqual(inputBlock2, outputBlock2);
+            Assert.AreEqual(inputBlock3, outputBlock3);
+        }
+    }
+}
---
+++ GZipLibrary.Tests/BlockReadWriteTest.cs
@@ -0,0 +1,70 @@
+﻿using System.IO;
+using GZipLibrary.Blocks;
+using GZipLibrary.Blocks.Readers;
+using GZipLibrary.Blocks.Writers;
+using NUnit.Framework;
+
+namespace GZipLibrary.Tests
+{
+    [TestFixture]
+    public class BlockReadWriteTest
+    {
+        [Test]
+        public void ReadUncompressedBlockFromStream()
+        {
+            //Arrange
+            byte[] data = {1, 1, 2, 3, 5, 8, 13};
+            var stream = new MemoryStream(data);
+            var reader = new UncompressedBlockReader(stream, data.Length);
+
+            //Act
+            var result = reader.Read(out Block newBlock);
+
+            //Assert
+            Assert.True(result);
+            Assert.AreEqual(data, newBlock.Data);
+        }
+
+        [Test]
+        public void WriteUncompressedBlockToStream()
+        {
+            //Arrange
+            byte[] data = { 1, 1, 2, 3, 5, 8, 13 };
+            Block block = new Block(0, data);
+            var stream = new MemoryStream();
+            var writer = new UncompressedBlockWriter(stream, data.Length);
+
+            //Act
+            writer.Write(block);
+            byte[] newData = new byte[stream.Length];
+            stream.Seek(0, SeekOrigin.Begin);
+            stream.Read(newData, 0, newData.Length);
+
+            //Assert
+            Assert.AreEqual(data, newData);
+        }
+
+        [Test]
+        public void ReadWriteBlock()
+        {
+            //Arrange
+            byte[] data = { 1, 1, 2, 3, 5, 8, 13 };            
+            var inputStream = new MemoryStream(data);
+            var outputStream = new MemoryStream();
+            BlockReader reader = new UncompressedBlockReader(inputStream, data.Length);
+            BlockWriter writer = new CompressedBlockWriter(outputStream);
+
+            //Act
+            var result1 = reader.Read(out Block block1);
+            writer.Write(block1);
+
+            outputStream.Seek(0, SeekOrigin.Begin);
+            reader = new CompressedBlockReader(outputStream);            
+            var result2 = reader.Read(out Block block2);
+
+            //Assert
+            Assert.True(result1 && result2);
+            Assert.AreEqual(data, block2.Data);
+        }
+    }
+}
